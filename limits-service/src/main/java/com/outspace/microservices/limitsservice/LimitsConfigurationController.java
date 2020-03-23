77a127bffc22cec226994f0982cf92a786601366
Marfall <marinefalling@gmail.com>
2020-03-23 18:32:26 +0300

    microservices ready

--- limits-service/src/main/java/com/outspace/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/outspace/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.outspace.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.outspace.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +16,16 @@ public class LimitsConfigurationController {
     public LimitConfiguration retrieveLimitsFromConfiguration() {
 
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(9,999);
     }
 }
